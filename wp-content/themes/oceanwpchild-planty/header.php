d8604835915b730a401ee6f816a03b73b5eee007
Pauline Ramelet <pauline.ramelet@icloud.com>
2025-08-21 14:01:49 +0200

    Création header.php et modifications CSS hover et focus du lien du footer

---
+++ wp-content/themes/oceanwpchild-planty/header.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * @package OceanWP WordPress theme
+ */
+
+?>
+<!DOCTYPE html>
+<html class="<?php echo esc_attr( oceanwp_html_classes() ); ?>" <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<!-- Police d'écriture Syne -->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Syne:wght@400..800&display=swap" rel="stylesheet">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?> <?php oceanwp_schema_markup( 'html' ); ?>>
+
+	<?php wp_body_open(); ?>
+
+	<?php do_action( 'ocean_before_outer_wrap' ); ?>
+
+	<div id="outer-wrap" class="site clr">
+
+		<a class="skip-link screen-reader-text" href="#main"><?php echo esc_html( oceanwp_theme_strings( 'owp-string-header-skip-link', false ) ); ?></a>
+
+		<?php do_action( 'ocean_before_wrap' ); ?>
+
+		<div id="wrap" class="clr">
+
+			<?php do_action( 'ocean_top_bar' ); ?>
+
+			<?php 
+				/*Désactivation header généré par défaut par le thème 
+			 	do_action( 'ocean_header' ); 
+			 	Remplacement par wp_nav_menu*/
+				wp_nav_menu([
+  					'theme_location' => 'menu-header-planty',
+					//Utilité de la suppression de div ?
+					'container' => false,
+					//Classe reportée dans le CSS du thème enfant
+   					'menu_class' => 'classheaderplanty'
+				]);
+			 ?>
+
+			<?php do_action( 'ocean_before_main' ); ?>
+
+			<main id="main" class="site-main clr"<?php oceanwp_schema_markup( 'main' ); ?> role="main">
+
+				<?php do_action( 'ocean_page_header' ); ?>
